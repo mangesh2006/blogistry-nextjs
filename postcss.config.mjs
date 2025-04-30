bcc08bddd9e5fb06673d9ad2f33379d5238c458b
Mangesh dev <deomangesh06@gmail.com>
2025-04-30 11:23:21 +0530

    second commit

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,8 @@
 import tailwindcss from "tailwindcss";
-import autoprefixer from "autoprefixer";
 
 /** @type {import('postcss').ProcessOptions} */
 const config = {
-  plugins: [tailwindcss, autoprefixer],
+  plugins: [tailwindcss],
 };
 
 export default config;
