b338118933c888b88542049b80ed65606aa93700
Mangesh dev <deomangesh06@gmail.com>
2025-04-30 10:28:38 +0530

    updated

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,6 @@
 import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
 import typography from '@tailwindcss/typography'
 
 export default {
-  plugins: [tailwindcss, autoprefixer, typography],
+  plugins: [tailwindcss, typography],
 }
