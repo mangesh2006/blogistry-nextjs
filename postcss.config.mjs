1521f69a3b9fc9f40f84b93bcdb240bbc655250e
Mangesh dev <deomangesh06@gmail.com>
2025-04-30 10:33:14 +0530

    updated

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,8 @@
 import tailwindcss from 'tailwindcss'
-import typography from '@tailwindcss/typography'
 
 export default {
   plugins: {
     tailwindcss: {},
-    typography: {}
+    '@tailwindcss/typography': {},
   },
 }
