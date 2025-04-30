0f487fe4bb74ea494c916bd7b7b9be86b4d8e2ec
Mangesh dev <deomangesh06@gmail.com>
2025-04-30 10:26:18 +0530

    updated

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,7 @@
-const config = {
-  plugins: ["@tailwindcss/postcss", "@tailwind/typography",],
-};
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+import typography from '@tailwindcss/typography'
 
-export default config;
+export default {
+  plugins: [tailwindcss, autoprefixer, typography],
+}
